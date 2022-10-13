da0b28015c13babd6cb5688bf8556a8751e34484
Elkennard <100434303+Elkennard@users.noreply.github.com>
2022-10-13 16:34:07 +0100

    created modal for techniques explanation

--- script.js
+++ script.js
@@ -138,7 +138,7 @@ function Animate() {
   if (position <= 11) {
     numberOfFrames = 4;
     framesPerSecond = document.getElementById("breathIn").value;
-    timerText.innerHTML = "BREATH IN";
+    timerText.innerHTML = "BREATHE IN";
   }
   if (position >= 12 && position < 14) {
     numberOfFrames = 1;
@@ -148,7 +148,7 @@ function Animate() {
   if (position >= 15) {
     numberOfFrames = 12;
     framesPerSecond = document.getElementById("breathOut").value;
-    timerText.innerHTML = "BREATH OUT";
+    timerText.innerHTML = "BREATHE OUT";
   }
   if (position === 24) {
     position = 0;
@@ -157,3 +157,29 @@ function Animate() {
 Animate();
 
 requestAnimationFrame(Animate);
+
+// Get the modal
+const modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+const btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+const span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
